9119127e8d465d2fda11f183bd8827b64ec4f812
Kelthon <kelthonbalbino@gmail.com>
2024-04-29 18:13:36 -0300

    feat: added new component MenuBar, fixed format problems, removed styles from App.css

---
+++ src/components/MenuBar.tsx
@@ -0,0 +1,15 @@
+import "./MenuBar.css";
+
+export default function MenuBars(): JSX.Element {
+  return (
+    <header id="menu-bar">
+      <ul>
+        <li>file</li>
+        <li>edit</li>
+        <li>view</li>
+        <li>window</li>
+        <li>help</li>
+      </ul>
+    </header>
+  );
+}
