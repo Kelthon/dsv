ae610e3012e580ed10b9560c70e386c6262dea62
Kelthon <kelthonbalbino@gmail.com>
2024-05-03 14:34:47 -0300

    style: added padding, removed App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Canvas from "./components/Canvas";
 import MenuBars from "./components/MenuBar";
 
 function App(): JSX.Element {
   return (
     <>
       <MenuBars />
+      <Canvas />
     </>
   );
 }
