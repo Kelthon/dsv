5101b6d6b0e55d5b8f87f44a5d094476760c04e8
Kelthon <kelthonbalbino@gmail.com>
2024-06-08 14:45:17 -0300

    feat: added relative import to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import MenuBars from "./components/MenuBar/MenuBar";
-import StatusBar from "./components/StatusBar/StatusBar";
-import ToolBar from "./components/ToolBar/ToolBar";
-import Workspace from "./components/Workspace/Workspace";
-import { ViewportProvider } from "./hooks/Viewport";
+import MenuBars from "src/components/MenuBar/MenuBar";
+import { StatusBar } from "src/components/StatusBar";
+import ToolBar from "src/components/ToolBar/ToolBar";
+import Workspace from "src/components/Workspace/Workspace";
+import { ViewportProvider } from "src/hooks/Viewport";
 
 function App(): JSX.Element {
   return (
